6a228049ac0aaf10324a8ffcf990c05f451cd816
Ullrika <ullrika.sahlin@cec.lu.se>
2023-08-28 12:45:16 +0200

    first commit

---
+++ data/radiation.R
@@ -0,0 +1,5 @@
+Japan-data  + Kod från blåaboken erhållen av David från USEPA
+
+David delade artikel där man tagit hänsyn till osäkerhet i dos PDF
+
+Reviewartikel med data https://royalsocietypublishing-org.ludwig.lub.lu.se/doi/10.1098/rspb.2017.1070
