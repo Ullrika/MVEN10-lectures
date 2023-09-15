b102db32ab0008fbb7433907336f6c5d91df7a78
Ullrika <ullrika.sahlin@cec.lu.se>
2023-09-15 10:59:46 +0200

    ip

---
+++ pages/case_studies_1.R
@@ -0,0 +1,7 @@
+exp_av = c(0.03,0.15)
+exp_P95 = c(0.07,0.33)
+RP = 0.06
+
+RP/exp_av
+
+RP/exp_P95
---
+++ pages/test of seir.R
@@ -0,0 +1,11 @@
+install.packages("EpiDynamics")
+library(EpiDynamics)
+
+
+# Parameters and initial conditions.
+parameters <- c(mu = 1 / (70 * 365), beta = 520 / 365,
+                sigma = 1 / 14, gamma = 1 / 7)
+initials <- c(S = 0.1, E = 1e-04, I = 1e-04, R = 1 - 0.1 - 1e-4 - 1e-4)
+# Solve and plot.
+seir <- SEIR(pars = parameters, init = initials, time = 0:(60 * 365))
+PlotMods(seir)
